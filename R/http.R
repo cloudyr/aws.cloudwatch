307ef5286f646d6e9937b2b1f8aa11fcd5d18013
Thomas J. Leeper <thosjleeper@gmail.com>
2018-04-14 17:06:56 +0100

    cleanup http code & fix session_token arg

--- R/http.R
+++ R/http.R
@@ -3,11 +3,12 @@
 #' @details This is the workhorse function to execute CloudWatch API requests. It should not normally be used directly.
 #' @param query A list.
 #' @param api A character string specifying which CloudWatch API to call. One of \dQuote{monitoring}, \dQuote{events}, or \dQuote{logs}.
-#' @param headers A list of request headers for the REST call.   
+#' @param headers A list of headers to pass to the HTTP request.
+#' @param verbose A logical indicating whether to be verbose. Default is given by \code{options("verbose")}.
 #' @param region A character string containing the AWS region. If missing, defaults to \dQuote{us-east-1}.
-#' @param key A character string containing an AWS Access Key ID. If missing, defaults to value stored in environment variable \dQuote{AWS_ACCESS_KEY_ID}.
-#' @param secret A character string containing an AWS Secret Access Key. If missing, defaults to value stored in environment variable \dQuote{AWS_SECRET_ACCESS_KEY}.
-#' @param session_token Optionally, a character string containing an AWS temporary Session Token. If missing, defaults to value stored in environment variable \dQuote{AWS_SESSION_TOKEN}.
+#' @param key A character string containing an AWS Access Key ID. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param secret A character string containing an AWS Secret Access Key. See \code{\link[aws.signature]{locate_credentials}}.
+#' @param session_token A character string containing an AWS Session Token. See \code{\link[aws.signature]{locate_credentials}}.
 #' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
 #' @import httr
 #' @importFrom jsonlite fromJSON
@@ -15,14 +16,25 @@
 #' @importFrom aws.signature signature_v4_auth
 #' @export
 cloudwatchHTTP <- 
-function(query, 
-         api = c("monitoring", "events", "logs"),
-         headers = list(), 
-         region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"), 
-         key = Sys.getenv("AWS_ACCESS_KEY_ID"), 
-         secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"), 
-         session_token = Sys.getenv("AWS_SESSION_TOKEN"),
-         ...) {
+function(
+  query, 
+  api = c("monitoring", "events", "logs"),
+  headers = list(), 
+  verbose = getOption("verbose", FALSE),
+  region = Sys.getenv("AWS_DEFAULT_REGION","us-east-1"),
+  key = NULL,
+  secret = NULL,
+  session_token = NULL,
+  ...
+) {
+    # locate and validate credentials
+    credentials <- locate_credentials(key = key, secret = secret, session_token = session_token, region = region, verbose = verbose)
+    key <- credentials[["key"]]
+    secret <- credentials[["secret"]]
+    session_token <- credentials[["session_token"]]
+    region <- credentials[["region"]]
+    
+    # generate request signature
     current <- Sys.time()
     d_timestamp <- format(current, "%Y%m%dT%H%M%SZ", tz = "UTC")
     api <- match.arg(api)
@@ -39,15 +51,18 @@ function(query,
            request_body = "",
            key = key, 
            secret = secret,
-           session_token = session_token)
+           session_token = session_token,
+           verbose = verbose)
+    # setup request headers
     headers[["x-amz-date"]] <- d_timestamp
+    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     headers[["x-amz-content-sha256"]] <- Sig$BodyHash
     if (!is.null(session_token) && session_token != "") {
         headers[["x-amz-security-token"]] <- session_token
     }
-    headers[["Authorization"]] <- Sig[["SignatureHeader"]]
     H <- do.call(add_headers, headers)
 
+    # execute request
     r <- GET(paste0("https://", api, ".amazonaws.com"), H, query = query, ...)
     if (http_status(r)$category == "Client error") {
         x <- try(xml2::as_list(xml2::read_xml(content(r, "text", encoding = "UTF-8"))), silent = TRUE)
